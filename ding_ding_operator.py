284ac2d2a2ea865857b356b02baa7b9b171ed4ba
辣橙 <68316902+laorange@users.noreply.github.com>
2022-07-08 16:41:15 +0800

    feat: 对复杂部门结构的适应
    feat:
    + 自动生成 settings.json，存储当次信息
    + 自动读取 settings.json，加载已有信息
    
    docs: requirements.txt

--- ding_ding_operator.py
+++ ding_ding_operator.py
@@ -1,22 +1,70 @@
 import json
-from typing import List, TypedDict
+from typing import List, TypedDict, Dict, Tuple
+from tqdm import tqdm
 
 import requests
 
+SETTING_JSON_FILE = "settings.json"
 
-class DingUserInfo(TypedDict):
+
+class DingUserInfoDict(TypedDict):
     name: str
     userid: str
 
 
+class DepartmentDict(TypedDict):
+    ext: str  # --------------------------- "ext": "{\"faceCount\":\"int\"}",
+    auto_add_user: bool  # ---------------- "auto_add_user": true,
+    parent_id: int  # --------------------- "parent_id": 1,
+    name: str  # -------------------------- "name": "xx部门",
+    dept_id: int  # ----------------------- "dept_id": int,
+    create_dept_group: bool  # ------------ "create_dept_group": true
+
+
 class DingDingOperator:
-    def __init__(self, agentId, appKey, appSecret):
-        self.appSecret = appSecret
-        self.appKey = appKey
-        self.agentId = agentId
+    def __init__(self):
+        try:
+            with open(SETTING_JSON_FILE, encoding="utf-8") as settings_json:
+                settings = json.load(settings_json)
+        except FileNotFoundError:
+            settings = dict()
+
+        agentId = settings.get("AGENT_ID", None)
+        appKey = settings.get("APP_KEY", None)
+        appSecret = settings.get("APP_SECRET", None)
+
+        self.agentId: str = agentId if agentId else input("请输入AgentId: ")
+        self.appKey: str = appKey if appKey else input("请输入AppKey: ")
+        self.appSecret: str = appSecret if appSecret else input("请输入AppSecret: ")
+
+        departmentIdList = settings.get("PRESET_DEPARTMENTS", None)
+        userDict = settings.get("PRESET_MEMBERS", None)
 
         self.accessToken = self.getAccessToken()
-        self.users: List[DingUserInfo] = self.getUserListSimple()
+
+        self.departmentIdList = departmentIdList if departmentIdList else self.getDescendantDepartmentIdList()
+        self.userDict = userDict if userDict else self.getFullUser()
+        self.settings = {
+            "AGENT_ID": self.agentId,
+            "APP_KEY": self.appKey,
+            "APP_SECRET": self.appSecret,
+            "PRESET_DEPARTMENTS": self.departmentIdList,
+            "PRESET_MEMBERS": self.userDict
+        }
+        with open(SETTING_JSON_FILE, "wt", encoding="utf-8") as file:
+            json.dump(self.settings, file, ensure_ascii=False)
+
+    @staticmethod
+    def getDingTalkResponse(method, url, params, data=None):
+        if method == "GET":
+            response = requests.get(url, params=params).json()
+        elif method == "POST":
+            response = requests.post(url, params=params, data=data).json()
+        else:
+            raise Exception("暂不支持别的请求方式")
+        if response.get("errcode", -1) != 0:
+            raise Exception(response.get("errmsg", str(response)))
+        return response
 
     def getAccessToken(self) -> str:
         url = "https://oapi.dingtalk.com/gettoken"
@@ -24,22 +72,66 @@ class DingDingOperator:
         accessToken = requests.get(url, params=params).json()["access_token"]
         return accessToken
 
-    def getUserListSimple(self, dept_id=1, cursor=0, size=100) -> List[DingUserInfo]:
+    def getUserListSimple(self, dept_id=1, cursor=0, size=100) -> List[DingUserInfoDict]:
         url = "https://oapi.dingtalk.com/topapi/user/listsimple"
         params = dict(access_token=self.accessToken)
         data = dict(dept_id=dept_id, cursor=cursor, size=size)
-        response = requests.post(url, params=params, data=data).json()
-        if response.get("errcode", -1) != 0:
-            raise Exception(response.get("errmsg", str(response)))
-        userList: List[DingUserInfo] = response["result"]["list"]
+        response = self.getDingTalkResponse("POST", url, params, data)
+        userList: List[DingUserInfoDict] = response["result"]["list"]
 
         if response["result"]["has_more"]:
             userList = userList + self.getUserListSimple(dept_id=dept_id, cursor=cursor + 1, size=size)
 
         return userList
 
-    def filterUsers(self, givenUserNames: List[str]) -> List[DingUserInfo]:
-        return [user for user in self.users if (user["name"] in givenUserNames)]
+    def getSubDepartmentIdList(self, departmentId: int = 1) -> List[int]:
+        url = "https://oapi.dingtalk.com/topapi/v2/department/listsubid"
+        params = dict(access_token=self.accessToken)
+        data = dict(dept_id=departmentId)
+        response = self.getDingTalkResponse("POST", url, params, data)
+        return response["result"]["dept_id_list"]
+
+    def getDescendantDepartmentIdList(self, departmentId: int = 1) -> List[int]:
+        descendantDepartmentIdList = self.getSubDepartmentIdList(departmentId)
+        if len(descendantDepartmentIdList) != 0:
+            for descendantDepartmentId in tqdm(descendantDepartmentIdList, desc="正在获取部门信息"):
+                descendantDepartmentIdList += self.getSubDepartmentIdList(descendantDepartmentId)
+        return descendantDepartmentIdList + [1]  # 根部门 id=1
+
+    def getDepartmentName(self, departmentId: int = 1) -> str:
+        url = "https://oapi.dingtalk.com/topapi/v2/department/get"
+        params = dict(access_token=self.accessToken)
+        data = dict(dept_id=departmentId)
+        response = self.getDingTalkResponse("POST", url, params, data)
+        return response["result"]["name"]
+
+    def getDepartmentIdListOfUser(self, userId: str) -> List[int]:
+        url = "https://oapi.dingtalk.com/topapi/v2/user/get"
+        params = dict(access_token=self.accessToken)
+        data = dict(userid=userId)
+        response = self.getDingTalkResponse("POST", url, params, data)
+        return response["result"]["dept_id_list"]
+
+    def getDepartmentNameListOfUser(self, userId: str) -> List[str]:
+        departmentIdList = self.getDepartmentIdListOfUser(userId)
+        return [self.getDepartmentName(departmentId) for departmentId in departmentIdList]
+
+    def getFullUser(self):
+        userDict: Dict[str, str] = {}
+        for departmentId in tqdm(self.departmentIdList, desc="正在获取部门成员信息"):
+            userInfoList = self.getUserListSimple(dept_id=departmentId)
+            for userInfo in userInfoList:
+                if previousUserId := userDict.get(userInfo["name"], None):
+                    if previousUserId != userInfo["userid"]:
+                        print(f"\n\n警告: 发现相同名称的成员!")
+                        print(f"{userInfo['name']}({previousUserId}), 部门：{self.getDepartmentNameListOfUser(previousUserId)}")
+                        print(f"{userInfo['name']}({userInfo['userid']})，部门：{self.getDepartmentNameListOfUser(userInfo['userid'])}\n")
+                        continue
+                userDict[userInfo["name"]] = userInfo["userid"]
+        return userDict
+
+    def filterUsers(self, givenUserNames: List[str]) -> list[Tuple[str, str]]:
+        return [(userName, self.userDict[userName]) for userName in givenUserNames if userName in self.userDict]
 
     def sent_text_msg(self, user_id_list: List[str], text: str):
         """
@@ -48,6 +140,8 @@ class DingDingOperator:
         :param user_id_list : 发送目标的id
         :return post请求后的结果
         """
+        if not user_id_list:
+            return print("发送名单为空！")
         url = "https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2"
         params = dict(access_token=self.accessToken)
 
@@ -60,4 +154,5 @@ class DingDingOperator:
         if response.get("errcode", -1) != 0:
             raise Exception(f"请求错误：{response.get('errmsg', str(response))}")
         else:
+            print("发送完成!")
             return response
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import json
-
 from ding_ding_operator import DingDingOperator
 
-with open("settings.json", encoding="utf-8") as settings_json:
-    settings = json.load(settings_json)
+"""
+钉钉应用类型：企业内部开发
+需要在钉钉后台开启如下权限：
++ 通讯录管理：
+    + 通讯录部门信息读权限
+    + 成员信息读权限
+    + 通讯录部门成员读权限
+"""
 
-if __name__ == '__main__':
-    text = input("请输入想要发送的信息: ")
-    users = []
+
+def get_filtered_users():
+    _users = []
     while 1:
         new_user = input("请输入接收者的姓名: ")
         if new_user:
-            users.append(new_user)
-            print("(若已完成输入，请直接回车)")
+            _users.append(new_user)
+            print("(若已完成输入，请直接回车)", end="")
         else:
             break
-    operator = DingDingOperator(settings["AGENT_ID"], settings["APP_KEY"], settings["APP_SECRET"])
+    return _users
+
+
+if __name__ == '__main__':
+    operator = DingDingOperator()
+
+    text = input("请输入想要发送的信息: ")
+    users = get_filtered_users()
+
     filtered_users = operator.filterUsers(users)
-    print(f'根据您的输入，成功检索到了这些用户：{[user["name"] for user in filtered_users]}')
-    print(f'即将向他们发送"{text}"')
-    filtered_user_id_list = [user["userid"] for user in filtered_users]
-    print(operator.sent_text_msg(filtered_user_id_list, text))
+
+    if filtered_users:
+        print(f'根据您的输入，成功检索到了这些用户：{",".join([user[0] for user in filtered_users])}')
+        print(f'即将向他们发送"{text}"')
+        filtered_user_id_list = [user[1] for user in filtered_users]
+        send_feedback = operator.sent_text_msg(filtered_user_id_list, text)
+        print(send_feedback)
+    else:
+        print("没有找到符合条件的用户，请查证后重试")
+    input("请敲击回车来结束程序：")
