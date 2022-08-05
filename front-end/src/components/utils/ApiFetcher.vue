653ab17f42f8f63f169906eafee7af68302dd576
辣橙 <68316902+laorange@users.noreply.github.com>
2022-08-05 18:24:43 +0800

    perf: 持续在线5分钟后，刷新一次accessToken

--- front-end/src/components/utils/ApiFetcher.vue
+++ front-end/src/components/utils/ApiFetcher.vue
@@ -7,16 +7,27 @@ import {useMessage} from "naive-ui";
 const store = useStore();
 const message = useMessage();
 
+const handlers = {
+  async fetchAccessToken() {
+    store.accessToken = (await (await fetch((import.meta.env.VITE_BACKEND_URL ?? "") + "/access-token/")).json())["accessToken"] as string;
+  },
+  async fetchAddressBook() {
+    store.addressBook = await (await fetch((import.meta.env.VITE_BACKEND_URL ?? "") + "/address-book/")).json() as AddressBook;
+  },
+};
+
 onMounted(async () => {
   try {
     store.isLoading = true;
-    store.accessToken = (await (await fetch((import.meta.env.VITE_BACKEND_URL ?? "") + "/access-token/")).json())["accessToken"] as string;
-    store.addressBook = await (await fetch((import.meta.env.VITE_BACKEND_URL ?? "") + "/address-book/")).json() as AddressBook;
+    await handlers.fetchAccessToken();
+    await handlers.fetchAddressBook();
   } catch (e) {
     message.error("请求数据失败！请检查后端服务是否已被关闭");
   } finally {
     store.isLoading = false;
   }
+
+  setInterval(() => handlers.fetchAccessToken(), 1000 * 60 * 5); // 每5分钟刷新一次accessToken
 });
 </script>
 
